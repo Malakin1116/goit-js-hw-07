5cef66bda4ccd12daf91b673093a99cdaba4100f
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2023-09-17 12:51:43 +0300

    7-2 start

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const ulList = document.querySelector(".gallery");
+
+const renderList = (arr, container) => { 
+  const markup = arr.map((item) => `
+    <li class="gallery_item" > 
+      <a class="gallery_link" href="${item.original}">
+        <img
+          class="gallery_image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+          width="372px"
+        />
+      </a>
+    </li>`).join("");
+  
+  container.insertAdjacentHTML("afterbegin", markup);
+}
